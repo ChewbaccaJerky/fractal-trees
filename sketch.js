3c5f3093f1c11d073b5d5360b5c71f8eac290585
John Robin <johnrobinlugtu@gmail.com>
2018-06-13 23:53:59 -0700

    Added sliders for all changing variables

--- sketch.js
+++ sketch.js
@@ -1,20 +1,33 @@
-var angle = 0;
-var color = 0;
-var slider;
+var angle = 0,
+    color1 = 0,
+    color2 = 0,
+    color3 = 0,
+    len = 0;
+var redSlider = 0,
+    blueSlider = 0,
+    greenSlider = 0,
+    angleSlider = 0,
+    lenSlider = 0;
 
 function setup(){
     createCanvas(600, 600);
-    slider = createSlider(0, TWO_PI, PI/2, 0.1);
-    // console.log(Math.floor(Math.random() * 256));
+    angleSlider = createSlider(0, TWO_PI, TWO_PI, 0.1);
+    blueSlider = createSlider(0, 256, 0, 1);
+    redSlider = createSlider(0, 256, 0, 1);
+    greenSlider = createSlider(0, 256, 0, 1);
+    lenSlider = createSlider(0, 100, 10, 1);
 }
 
 function draw(){
     background(50);
-    angle = slider.value();
-    // color = Math.floor(Math.random() * 256);
-    stroke(Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256));
+    angle = angleSlider.value();
+    color1 = blueSlider.value();
+    color2 = redSlider.value();
+    color3 = greenSlider.value();
+    len = lenSlider.value();
+    stroke(color1, color2, color3);
     translate(width/2, height - height/4);
-    branch(85);
+    branch(len);
 }
 
 function branch(len) {
