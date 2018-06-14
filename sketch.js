65d4c08755613f128acb329323926df290c19f2e
John Robin <johnrobinlugtu@gmail.com>
2018-06-14 14:13:45 -0700

    Added animation and random color button

--- sketch.js
+++ sketch.js
@@ -1,13 +1,17 @@
-var angle = 0,
-    color1 = 0,
-    color2 = 0,
-    color3 = 0,
-    len = 0;
-var redSlider = 0,
-    blueSlider = 0,
-    greenSlider = 0,
-    angleSlider = 0,
-    lenSlider = 0;
+var angle,
+    color1,
+    color2,
+    color3,
+    len,
+    animate;
+
+var redSlider,
+    blueSlider,
+    greenSlider,
+    angleSlider,
+    lenSlider,
+    animateBtn,
+    randColor;
 
 function setup(){
     let sliderContainer = document.getElementsByClassName('slider-container')[0];
@@ -17,30 +21,61 @@ function setup(){
     redSlider = document.getElementById('r');
     greenSlider = document.getElementById('g');
     blueSlider = document.getElementById('b');
+    animateBtn = document.getElementById('animate');
+    randColor = document.getElementById('rand-color');
 }
 
 function draw(){
     background(256);
-    angle = parseFloat(angleSlider.value);
-    color1 = parseInt(blueSlider.value);
-    color2 = parseInt(redSlider.value);
-    color3 = parseInt(greenSlider.value);
+    if(animateBtn.checked) {
+        angle += 0.01;
+    }
+    else {
+        angle = parseFloat(angleSlider.value);
+    }
+
+    if(randColor.checked) {
+        color1 = Math.floor(Math.random() * 256);
+        // redSlider.value = color1;
+        color2 = Math.floor(Math.random() * 256);
+        // greenSlider.value = color2;
+        color3 = Math.floor(Math.random() * 256);
+        // blueSlider.value = color3;
+    }
+    else {
+        color1 = parseInt(redSlider.value);
+        color2 = parseInt(greenSlider.value);
+        color3 = parseInt(blueSlider.value);
+    }
+    
     len = parseInt(lenSlider.value);
     stroke(color1, color2, color3);
     translate(width/2, height - height/4);
-    branch(len);
+    fractalBranch(len);
+
+    // switch(fractal) {
+    //     case "Fractal Tree":
+    //         fractalBranch(len);
+    //         break;
+
+    //     case "L-System":
+    //         break;
+
+    //     default:
+    //         break;
+    // }
 }
 
-function branch(len) {
+function fractalBranch(len) {
     if(len < 5) return;
     line(0, 0, 0, -len);
     translate(0, -len);
     push();
     rotate(angle);
-    branch(len * 0.78);
+    fractalBranch(len * 0.78);
     pop();
     push();
     rotate(angle * -1);
-    branch(len * 0.78);
+    fractalBranch(len * 0.78);
     pop();
 }
